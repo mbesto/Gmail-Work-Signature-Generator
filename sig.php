44cd5b88a567512d3b82ef6fe1c59a0e09993ad5
Mike Bestvina <mbesto@gmail.com>
2012-01-03 17:41:43 -0600

    second commit

---
+++ sig.php
@@ -0,0 +1,35 @@
+<html>
+<body>
+
+<div style="font-family:Calibri;font-size:11pt"><p><b style="font-size:12pt;color:#57B6F0"><?php echo $_POST["fullname"]; ?></b>
+<br />
+<span style="color:#004E9E"><?php echo $_POST["title"]; ?></span><br />
+<span style="color:#858585">
+<b>Mobile</b> <?php echo $_POST["mobile"]; ?><br />
+<b>Skype</b> <?php echo $_POST["skype"]; ?><br />
+<b>Email</b> <?php echo $_POST["email"]; ?> <br />
+</span>
+</p>
+<p><img src="http://xoomworks.com/email_sig/bar.jpg" />
+</p>
+    <p><img src="http://xoomworks.com/email_sig/xoomworks_logo.gif" />
+</p>
+
+<p><b style="color:#57B6F0"><br />Xoomworks Consulting and Outsourcing</b><br />
+<span style="color:#858585">Dunstan House | 14a St Cross Street | London EC1N 8XA<br />
+    <b>Tel</b> +44 (0) 20 7400 6120 <b>Web </b><a style="color:#858585" href="http://www.xoomworks.com">www.xoomworks.com</a></span></p>
+
+<p>
+    <a href="http://twitter.com/xoomworks"><img src="http://xoomworks.com/email_sig/follow_xoomworks.gif" /></a>
+    <a href="http://www.xoomworks.com"><img src="http://xoomworks.com/email_sig/linkedin.gif" /></a>
+    <a href="http://www.linkedin.com/company/xoomworks"><img src="http://xoomworks.com/email_sig/twitter.gif" /></a>
+</p>
+
+    <br />
+
+<p style="font-size:9pt;color:#B5B0A0">Xoomworks is a professional services company that specialises in Procurement Technology, Business Intelligence solutions and the design and development of complex bespoke software solutions. Our Consulting and Outsourcing divisions have an impressive record of delivering successful projects and services to blue chip and public-sector clients globally. Xoomworks provides consulting, systems integration, application managed services and offshore development services to many of the FTSE 100 companies.</p>
+
+<p style="font-size:9pt;color:#B5B0A0"><br />
+The information in this email is confidential. It is intended solely for the addressee. Access to this email by anyone else is unauthorised. If you are not the intended recipient, any disclosure, copying, distribution or any action taken or omitted to be taken in reliance on it, is prohibited and may be unlawful.</p>
+</div>
+</body>
